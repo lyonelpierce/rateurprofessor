60d280b67513f73dacf665048a549ffa03a53211
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2023-11-11 17:59:58 -0500

    Fix import order in Universidad page component

--- app/(profile)/universidad/[id]/page.tsx
+++ app/(profile)/universidad/[id]/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import useSWR from "swr";
-import { notFound } from "next/navigation";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
