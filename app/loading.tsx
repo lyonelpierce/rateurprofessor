f49d60c48be3c75ebad46e0a20f80ca7e48c30d6
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2023-11-17 22:21:23 -0500

    remove ui ball

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { NewtonsCradle } from "@uiball/loaders";
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-full w-full z-50">
-      <NewtonsCradle size={40} speed={1.4} color="black" />
-    </div>
-  );
-}
