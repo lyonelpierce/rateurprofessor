77941228d4fbbe9834550fc4fa93391277511e0c
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2023-11-18 17:13:29 -0500

    meta title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { esES } from "@clerk/localizations";
 
 export const metadata: Metadata = {
-  title: "Califica Tu Profe",
+  title: "Califica Tu Profe - Califica Profesores y Universidades de Ecuador",
   description: "Califica a Profesores y Universidades de Ecuador",
   icons: {
     icon: "/Logo.ico",
