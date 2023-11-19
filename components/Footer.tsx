acbce58bee35dc6b50266b6e694fea12292e629f
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2023-11-19 10:42:07 -0500

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,33 @@
-import Image from "next/image";
 import Link from "next/link";
+
+import { Instagram, Facebook, Twitter } from "lucide-react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="relative w-full bg-black p-4 text-white h-22">
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row gap-4 items-center justify-between">
+    <footer className="relative w-full bg-black p-4 text-white">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row gap-4 items-center justify-between h-12">
         <p className="font-medium text-sm text-center">
           &copy; {currentYear} Style593 LLC, Todos los derechos reservados.
         </p>
-        <Link href="/">
-          <Image src="/Logo.svg" width={50} height={50} alt="Logo Emoji" />
-        </Link>
+        <ul className="flex gap-4">
+          <li>
+            <Link href="#">
+              <Instagram size={20} />
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <Facebook size={20} />
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <Twitter size={20} />
+            </Link>
+          </li>
+        </ul>
       </div>
     </footer>
   );
